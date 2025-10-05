90ddc00525aa73227870f84a3742b8822f96536e
Mike Fechner <mike.fechner@consultingwerk.de>
2025-10-05 08:45:38 +0200

    Added auth key

--- inspect.cmd
+++ inspect.cmd
@@ -1,4 +1,5 @@
 @echo off
 rem npx @modelcontextprotocol/inspector dotnet run --project smartframeworkmcpserver --pasoeUrl https://sfrbo.consultingwerkcloud.com:8821/apsv
 
-npx @modelcontextprotocol/inspector smartframeworkmcpserver\bin\Debug\net8.0\smartframeworkmcpserver.exe --pasoeUrl https://sfrbo.consultingwerkcloud.com:8821/apsv
+npx @modelcontextprotocol/inspector sports2000mcpserver\bin\Debug\net8.0\win-x64\sports2000mcpserver.exe --pasoeUrl https://sfrbo.consultingwerkcloud.com:8821/apsv --authKey rTvpPQQq8Sjq6jJ4OqcvuAYEUgfk/eLFQUeWDd+SOM/41VQtq3ROzNnc9jA91nxcLKbNOESTdk0TlDh/o4HtL6NDCH0FJ6gI9Kxj39B3BxX4P7fVvCnxJEjM/Us+SBj5
+
